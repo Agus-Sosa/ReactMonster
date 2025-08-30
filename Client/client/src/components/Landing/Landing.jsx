3eabe4f68c392b3639c4e8cd69b06a83d6367ddc
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-08-30 12:55:53 -0300

    fixed

---
+++ Client/client/src/components/Landing/Landing.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Button from "@mui/material/Button";
+import { styled } from '@mui/material/styles';
+import "./Landing.css";
+import btnbg from '../../assets/img/test.png';
+import titulogame from "../../assets/img/title_game.png";
+
+function Landing() {
+  return (
+    <>
+    <section className='landing'>
+    <img className="title-img" src={titulogame} alt="Nombre del juego"/>
+
+    <h2 id='eslogan'>Domina la guerra, controla la carne, gobierna el mundo.</h2>
+
+    <Button className='btn-play'
+    sx={{
+    backgroundImage: `url(${btnbg})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    color: "yellow",
+    border: "1px solid red",
+    borderRadius: "5px",
+    padding: "20px 60px",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+<<<<<<< HEAD
+    textShadow: "-1px -1px 0 red,  1px -1px 0 red,-1px  1px 0 red,1px  1px 0 red; ",
+    "&:hover": {
+      backgroundBlendMode: "luminosity",
+      backgroundColor:'rgba(255, 0, 0, 0.95)',
+      textShadow: "-1px -1px 0 red,  1px -1px 0 red,-1px  2px 0 red,2px  2px 0 red; ",
+    },
+    
+=======
+    "&:hover": {
+      backgroundBlendMode: "luminosity",
+      backgroundColor:'rgba(255, 0, 0, 0.95)'
+    },
+    textShadow: "-1px -1px 0 red,  1px -1px 0 red,-1px  1px 0 red,1px  1px 0 red; "
+>>>>>>> 241f269352de9c814b0fe75a81802b0ebda6db95
+  }}
+    > JUGAR AHORA</Button>
+    
+    </section>
+    </>
+  )
+}
+
+export default Landing
--- Client/client/src/pages/Home.jsx
+++ Client/client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Landing from '../components/Landing/Landing'
 
 const Home = () => {
   return (
     <>
-    <Header/>
+    
+    <Landing/>
     <Footer/>
+    
     </>
 )
 }
