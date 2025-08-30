9618fb4cf1f6930c3b7730ba5f5102b2cb3c4ab5
Agustín Sosa <106622274+Agus-Sosa@users.noreply.github.com>
2025-08-30 17:33:34 -0300

    implementacion de header

---
+++ Client/client/src/components/Header/HeaderDesktop.jsx
@@ -0,0 +1,23 @@
+import Box from '@mui/material/Box'
+import React from 'react'
+import title_game from '../../assets/img/title_game.png'
+const HeaderDesktop = () => {
+  return (
+    <Box component="header" sx={{display:"flex", justifyContent:"space-between  ", alignItems:"center", backgroundColor:"#1e1e1e", color:"white", p:2}}>
+        <Box component="img" src={title_game} sx={{width:"100px"}}>
+        </Box>
+        <Box component="nav" sx={{display:"flex", gap:2, alignItems:"center"}}>
+        <Box component="a" sx={{backgroundColor:"#380E00", p:"10px" , border:"none", color:"white", cursor:"pointer", fontSize:"18px", borderRadius:"3px", transition:"all .5s", "&:hover":{backgroundColor:"#8E1616"}}}>
+            Foro
+        </Box>
+        <Box component="button" sx={{backgroundColor:"#8E1616", p:"10px" , border:"none", color:"white", cursor:"pointer", fontSize:"18px", borderRadius:"3px", transition:"all .5s", "&:hover":{backgroundColor:"#380E00"}}}>
+            Jugar Ahora
+        </Box>
+
+        </Box>
+
+    </Box>
+)
+}
+
+export default HeaderDesktop
