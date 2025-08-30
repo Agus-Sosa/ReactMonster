5a0dc627a54fcebdbdfe9cc27336c939c0a7d926
Agustín Sosa <106622274+Agus-Sosa@users.noreply.github.com>
2025-08-30 13:03:05 -0300

    footer

--- Client/client/src/components/Footer.jsx
+++ Client/client/src/components/Footer.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import Box from '@mui/material/Box'
+import title_game from '../assets/img/title_game.png'
+import Button from '@mui/material/Button'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box>
+      <img src={title_game} alt="" />
+      <Box>
+        <p>© 2025,React Monster , Inc. React Monsters, el logotipo de React Monsters y React Monsters Studios son marcas comerciales o marcas registradas de React Monster, Inc. en Argentina y/o en el resto del mundo. Todos los derechos reservados.</p>
+        <Button>
+          Volver al inicio
+        </Button>
+      </Box>
+
+    </Box>
   )
 }
 
