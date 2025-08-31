e0284d4a7c8f5f09e9e1565376b13c79f51e69dd
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-08-31 11:56:47 -0300

    edit  btn style

--- Client/client/src/components/Landing/Landing.jsx
+++ Client/client/src/components/Landing/Landing.jsx
@@ -6,6 +6,7 @@ import btnbg from '../../assets/img/test.png';
 import titulogame from "../../assets/img/title_game.png";
 import Box from '@mui/material/Box';
 
+
 function Landing() {
   return (
     <>
@@ -16,26 +17,30 @@ function Landing() {
 
     <Button className='btn-play'
     sx={{
-    backgroundImage: `url(${btnbg})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    color: "yellow",
-    border: "1px solid red",
-    borderRadius: "5px",
-    padding: "20px 60px",
-    fontWeight: "bold",
-    boxShadow: '2px 5px 5px black',
-    textTransform: "uppercase",
-    textShadow: "-1px -1px 0 black,  1px -1px 0 black,-1px  1px 0 black,1px  1px 0 black; ",
-    "&:hover": {
-      backgroundBlendMode: "luminosity",
-      backgroundColor:'rgba(255, 0, 0, 0.95)',
-      textShadow: "-1px -1px 0 black,  1px -1px 0 red,-1px  2px 0 red,2px  2px 0 black; ",
-      boxShadow: '1px 2px 2px black',
-    },
-    
-    
+        backgroundImage: `url(${btnbg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        color: "#ffea00",
+        border: "2px solid #ff0000",
+        borderRadius: "8px",
+        padding: "20px 60px",
+        fontWeight: "bold",
+        fontFamily: "'Creepster', cursive",
+        textTransform: "uppercase",
+        position: "relative",
+        overflow: "hidden",
+        boxShadow: "0 0 15px #ff4500, inset 0 0 20px #ff0000",
+        textShadow: "0 0 5px #ff0000, 0 0 10px #ff4500, 2px 2px 2px black",
+        transition: "all 0.3s ease-in-out",
+        "&:hover": {
+          animation: "fire-shake 10s",
+          textShadow: "0 0 15px #ffea00, 0 0 25px #ff4500, 2px 2px 2px black",
+          transform: "scale(1.08)",
+        },
+  }}
+  onClick={() => {
+    alert('El juego todavia no esta listo, aguanta(?');
   }}
     > JUGAR AHORA</Button>
     
