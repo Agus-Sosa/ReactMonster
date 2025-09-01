4a2265e51364ecc8d11086cc3839cdd420e5d109
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-09-01 20:08:45 -0300

    create foro

---
+++ Client/client/src/pages/Foro.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer'
+
+const Foro = () => {
+    return (
+        <>
+        <Header/>
+        <Footer/>
+        </>
+)
+}
+
+export default Foro
