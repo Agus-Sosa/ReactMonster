2b8d934176f35a41d562779bd193740feb762e14
Agustín Sosa <106622274+Agus-Sosa@users.noreply.github.com>
2025-09-02 14:12:25 -0300

    fix

--- Client/client/src/components/Foro/Foro-landing/Foro-landing.jsx
+++ Client/client/src/components/Foro/Foro-landing/Foro-landing.jsx
@@ -1,6 +1,3 @@
-import React from 'react'
-import Button from "@mui/material/Button";
-import { styled } from '@mui/material/styles';
 import "./Foro-landing.css";
 import foroImg from "../../../assets/img/foro-title.png";
 import Box from '@mui/material/Box';
--- Client/client/src/components/Header/HeaderDesktop.jsx
+++ Client/client/src/components/Header/HeaderDesktop.jsx
@@ -1,18 +1,16 @@
 import Box from '@mui/material/Box'
 import React from 'react'
 import title_game from '../../assets/img/title_game.png'
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const HeaderDesktop = () => {
-    const navigate = useNavigate();    
-        const irAlForo = () => {
-        navigate('/foro');};
+
   return (
     <Box component="header" sx={{display:"flex", justifyContent:"space-between  ", alignItems:"center", backgroundColor:"#1e1e1e", color:"white", p:2}}>
         <Box component="img" src={title_game} sx={{width:"100px"}}>
         </Box>
         <Box component="nav" sx={{display:"flex", gap:2, alignItems:"center"}}>
-        <Box component="a"  onClick={irAlForo} sx={{backgroundColor:"#380E00", p:"10px" , border:"none", color:"white", cursor:"pointer", fontSize:"18px", borderRadius:"3px", transition:"all .5s", "&:hover":{backgroundColor:"#8E1616"}}}>
-            Foro
+        <Box component={Link} to="/foro"   sx={{backgroundColor:"#380E00", p:"10px" , border:"none", color:"white", cursor:"pointer", fontSize:"18px", borderRadius:"3px", textDecoration:"none",transition:"all .5s", "&:hover":{backgroundColor:"#8E1616"}}}>
+            Comunidad
         </Box>
         <Box component="button" sx={{backgroundColor:"#8E1616", p:"10px" , border:"none", color:"white", cursor:"pointer", fontSize:"18px", borderRadius:"3px", transition:"all .5s", "&:hover":{backgroundColor:"#380E00"}}}>
             Jugar Ahora
