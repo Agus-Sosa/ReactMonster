14ce88fc7cecbe9ad5e06311d2943370920e5b55
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-09-02 12:02:30 -0300

    add foro landing
    agregue el landing del foro

---
+++ Client/client/src/components/Foro/Foro-landing/Foro-landing.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from "@mui/material/Button";
+import { styled } from '@mui/material/styles';
+import "./Foro-landing.css";
+import foroImg from "../../../assets/img/foro-title.png";
+import Box from '@mui/material/Box';
+
+
+function LandingForo() {
+  return (
+    <>
+    <Box component='section' className='landing-foro'>
+    <img className="foro-img" src={foroImg} alt="Nombre del juego"/> 
+    </Box>
+    </>
+  )
+}
+export default LandingForo
--- Client/client/src/pages/Foro.jsx
+++ Client/client/src/pages/Foro.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer'
+import LandingForo  from '../components/Foro/Foro-landing/Foro-landing'
 
 const Foro = () => {
+    
     return (
         <>
         <Header/>
+        <LandingForo/>
         <Footer/>
         </>
 )
