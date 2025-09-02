07cc7653f9c3d854205f8788c31ecafcc3f26bf1
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-09-02 15:17:18 -0300

    add  folds of the forum

--- Client/client/src/components/Forum/Forum-joins/Joins.jsx
+++ Client/client/src/components/Forum/Forum-joins/Joins.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import { Box, height } from '@mui/system'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
 
-function joins() {
+
+function Joins({informacion}) {
   return (
-    <Box></Box>
+    <Card sx={{ width:'75%', height:'95px', margin:'20px'}}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {informacion.titulo}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {informacion.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   )
 }
 
-export default joins
+export default Joins
