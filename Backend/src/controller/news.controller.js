f82e8ad1d7d115e1df1be4dea30fc3007d4aed4c
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-09-13 16:30:43 -0300

    Update news.controller.js

--- Backend/src/controller/news.controller.js
+++ Backend/src/controller/news.controller.js
@@ -6,7 +6,7 @@ class NewController {
     }
      async getAllNews (req, res) {
         try {
-            const news = await this.userService.getAllNews();
+            const news = await this.newService.getAllNews();
             res.status(200).json(news)
         } catch (error) {
             res.status(500).json({status: "error", message:"Error al obtener todas las noticias"})
@@ -16,11 +16,22 @@ class NewController {
     async getNewById(req, res, next){
         try {
             const id= req.params.id;
-            const news = await this.userService.getNewById(id);
+            const news = await this.newService.getNewById(id);
             res.status(200).json({status: "success", new:news});
         } catch (error) {
             next(error)
 
+        }
+
+    }
+        async deleteNewById (req, res) {
+        try {
+        
+            const {id}= req.params;
+            await this.newService.deleteNewById(id);
+            await res.status(200).json({status:"success", messgae:"Noticia eliminada con exito"})
+        } catch (error) {
+            next(error)
         }
     }
 
