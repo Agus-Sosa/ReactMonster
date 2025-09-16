2dc0f9c15d424c1814457905a79d0d5868e06d39
Agustín Sosa <106622274+Agus-Sosa@users.noreply.github.com>
2025-09-16 15:04:31 -0300

    agregacion de bcrypt (hashear el password)

--- Client/client/src/components/Authentication/Register.jsx
+++ Client/client/src/components/Authentication/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
     return (
         <>
         <Box component={Link} sx={{position:"absolute", top:0, left:0, textDecoration:"none", color:"white" , padding:1, m:1, background:"#8E1616", borderRadius:"4px"}} to="/">Volver atras</Box>
-    <Box sx={{backgroundColor:"#212121", color:"white", height:"100vh", display:'flex', justifyContent:"center", alignItems:"center"}}>
+        <Box sx={{backgroundColor:"#212121", color:"white", height:"100vh", display:'flex', justifyContent:"center", alignItems:"center"}}>
         
         <Box sx={{textAlign:"center", width:`${mobile ? '100%' : "30%"}`}}>
             <Box component="h1" sx={{color:"white", mb:10}}>
@@ -19,11 +19,11 @@ const Register = () => {
         </Box>
     
         <AuthForm  isForLogin={false}/>
-      <Box component="div"> 
+        <Box component="div"> 
             ¿Ya tienes una cuenta?  <Link to="/login" style={{textDecoration:"none", color:"#D84040"}}>Iniciar Sesion</Link>
         </Box>
         </Box>
-       
+
     </Box>
     </>
 )
