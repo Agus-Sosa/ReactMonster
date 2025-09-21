21072041a84ca028f0c083154ade28dff7b83e05
Agustín Sosa <106622274+Agus-Sosa@users.noreply.github.com>
2025-09-21 12:35:11 -0300

    fix

--- Client/client/src/pages/Foro.jsx
+++ Client/client/src/pages/Foro.jsx
@@ -1,6 +1,5 @@
 import React,{ useState } from 'react'
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer'
+
 import LandingForo  from '../components/Forum/Forum-landing/Forum-landing'
 import ForoJoins from '../components/Forum/Forum-joins/Forum-joins'
 import { useLocation } from 'react-router-dom';
@@ -15,10 +14,8 @@ const Foro = () => {
 
     return (
         <>
-        <Header/>
         <LandingForo/>
         <ForoJoins info={location} data={informacion}/>
-        <Footer/>
         </>
 )
 }
