8499bd6bb1980b529c5865f3ef1bcd31bb28efc3
Tomas Dev <152812185+TomasB-Dev@users.noreply.github.com>
2025-10-02 18:56:11 -0300

    add comment box without form to make one

--- Client/client/src/components/Forum/Forum-Comments/CommentsSection.jsx
+++ Client/client/src/components/Forum/Forum-Comments/CommentsSection.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { IconButton, Typography, Box } from "@mui/material";
 function CommentsSection() {
-  return (
+return (
     <>
-    <Box component="section"sx={{
-            width: "100%",  
-            height: "auto",}}>
-                <Box component="h4" sx={{fontSize:"1rem"}}>Comentarios</Box>
+    <Box
+    component="section"
+    sx={{
+        width: "100%",
+        border: "1px solid #ccc",  
+        borderRadius: 2,
+        padding: 2,
+        backgroundColor: "transparent"
+    }}
+    >
+        <Typography 
+            variant="subtitle1" 
+            sx={{ fontWeight: "bold", mb: 1 }}
+        >
+        Comentarios
+        </Typography>
+
+    
     </Box>
+
     </>
   )
 }
