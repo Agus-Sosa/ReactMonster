f2bc2e21e4b1f6cf9816e4c7aa9f48747e4068bd
Avlp21 <andyvolpe21@hotmail.com>
2025-10-15 11:08:51 -0300

    vista admin/user arena

--- Client/client/src/pages/Sands.jsx
+++ Client/client/src/pages/Sands.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect, useContext } from 'react';
 import ViewUser from '../components/ViewSand/ViewUser'
 import ViewAdmin from '../components/ViewSand/ViewAdmin';
+import { AuthContext } from '../context/AuthContext';
 
 
 
 const Sands = () => {
 
-  
+  const { user } = useContext(AuthContext);
+
+  if (!user) return <ViewUser />;
   return (
   <div>
     {/* <ViewUser/> */}
