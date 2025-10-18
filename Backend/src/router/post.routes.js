f6702e1b3b311fedac9153faf92a1fa807701ae1
IvoBertoni89 <bertoniivox@gmail.com>
2025-10-18 01:55:35 -0300

    jwt post router
    eso

--- Backend/src/router/post.routes.js
+++ Backend/src/router/post.routes.js
@@ -1,31 +1,35 @@
 import { Router } from "express";
 import PostController from "../controller/post.controller.js";
+import { verifyToken } from "../middleware/validateUser.js"; 
+import { validateNewPost } from "../middleware/validatePost.js";
 
 const router = Router();
-const postController=new PostController()
+const postController = new PostController();
 
-router.get('/', async(req, res, next)=>{
-    await postController.getAllPost(req,res, next)
-})
 
-router.get('/:id', async(req,res, next)=>{
-    await postController.getPostById(req,res,next);
-})
+router.get("/", async (req, res, next) => {
+  await postController.getAllPost(req, res, next);
+});
 
-router.post('/newPost', async(req,res,next)=>{
-    await postController.createNewPost(req, res, next);
-})
+router.get("/:id", async (req, res, next) => {
+  await postController.getPostById(req, res, next);
+});
 
-router.get('/categorias/:id', async(req,res,next)=>{
-    await postController.getPostByCategory(req, res, next);
-})
+router.get("/categorias/:id", async (req, res, next) => {
+  await postController.getPostByCategory(req, res, next);
+});
 
-router.delete('/:id', async(req,res,next)=>{
-    await postController.deletePost(req,res,next);
-})
+//JWT
+router.post("/newPost", verifyToken, validateNewPost, async (req, res, next) => {
+  await postController.createNewPost(req, res, next);
+});
 
-router.put('/:id', async(req,res,next)=>{
-    await postController.updatePost(req,res,next);
-})
+router.delete("/:id", verifyToken, async (req, res, next) => {
+  await postController.deletePost(req, res, next);
+});
 
-export {router as PostRouter};
+router.put("/:id", verifyToken, async (req, res, next) => {
+  await postController.updatePost(req, res, next);
+});
+
+export { router as PostRouter };
