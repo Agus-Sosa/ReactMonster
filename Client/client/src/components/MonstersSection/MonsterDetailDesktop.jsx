4085ea97aeebac6ed0de9e537bda6963c40357bc
Agustín Sosa <106622274+Agus-Sosa@users.noreply.github.com>
2025-10-19 19:15:02 -0300

    implmentacion de eliminacion en monster

--- Client/client/src/components/MonstersSection/MonsterDetailDesktop.jsx
+++ Client/client/src/components/MonstersSection/MonsterDetailDesktop.jsx
@@ -1,9 +1,20 @@
-import { Box } from '@mui/material'
+import { Box, Button } from '@mui/material'
 import React from 'react'
 import PageContainer from '../Layout/PageContainer/PageContainer'
 import hero_detail_monster from '../../assets/img/monsters/hero_detail_monster.png'
+import { useState } from 'react'
+import DeleteBtnModal from '../Buttons/BtnDelete/DeleteBtnModal'
 
-const MonsterDetailDesktop = ({monster}) => {
+const MonsterDetailDesktop = ({ monster, onDelete,userRole }) => {
+    const [openModal, setOpenModal] = useState(false);
+  const handleClose = () => { 
+    setOpenModal(false);
+  }
+  const handleOpenModal = () => { 
+    setOpenModal(true);
+  }
+
+
  return (
         <Box sx={{ maxHeight: "100vh", minHeight:"80vh",background: `url(${hero_detail_monster})`, backgroundPosition:"center", backgroundSize:"cover", overflowY:"hidden"}}>
             <PageContainer>
@@ -20,7 +31,14 @@ const MonsterDetailDesktop = ({monster}) => {
             width:"50%"
                     }}
                     >
-                        <Box component="h1" sx={{fontSize:{xs:"50px", md:"70px"}, color:'white', fontStyle:"italic"}}>{monster.monster_name}</Box>
+                     <Box component="h1" sx={{ fontSize: { xs: "50px", md: "70px" }, color: 'white', fontStyle: "italic" }}>{monster.monster_name}</Box>
+                     {
+                        (userRole === "admin" || userRole === "superadmin") &&
+                         <Button variant="contained" color="error" sx={{ mt: 3, mb: 1 }} onClick={handleOpenModal}>
+                            Eliminar
+                        </Button>
+                     } 
+                     <DeleteBtnModal onClose={handleClose} onDelete={onDelete} open={openModal} userRole={userRole}/>
                         <Box component="p" >
                             {monster.monster_description}
                         </Box>
